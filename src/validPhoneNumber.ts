ba1f399a170e3299f10a3aa54cf662973b35dc1d
Alexander Phillips <85855373+alexcphillips@users.noreply.github.com>
2021-07-26 14:58:28 -0400

    Update validPhoneNumber.ts

--- src/validPhoneNumber.ts
+++ src/validPhoneNumber.ts
@@ -1,3 +1,3 @@
-export function validPhoneNumber(phoneNumber: string) {
+export function validPhoneNumber(phoneNumber: string): boolean {
   return phoneNumber.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})?[-. ]?([0-9]{4})$/) ? true : false
 }
