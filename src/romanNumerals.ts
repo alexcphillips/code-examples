852ff7f7650868f6038c078562707a9bed4eec56
Alexander Phillips <alexphillipsdev@gmail.com>
2021-07-28 13:00:08 -0400

    Adding revised roman numeral helper

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,25 +1,5 @@
-// export class RomanNumerals {
-//   static numeralMap: { [char: string ]: any } = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1};
-
-//   static toRoman(number: number): string {
-//     let answer = ''
-//     for(let numeral in this.numeralMap) {
-//       while(number >= this.numeralMap[numeral]) {
-//         answer += numeral;
-//         number -= this.numeralMap[numeral];
-//       }
-//     }
-//     return answer;
-//   }
-//   static fromRoman(numerals : string): any {
-//     return numerals.match(/CM|CD|XC|XL|IX|IV|\w/g)?.reduce((acc, el) => acc + this.numeralMap[el], 0);
-//   }
-// }
-
-
-
 export class RomanNumerals {
-  static numeralMap: { [char: string ]: any } = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 };
+  static numeralMap: { [char: string ]: number } = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 };
   
   static toRoman(number: number): string {
     let answer = ''
@@ -36,28 +16,3 @@ export class RomanNumerals {
     return numeral.match(/CM|CD|XC|XL|IX|IV|\w/g)?.reduce((acc, el) => acc + this.numeralMap[el], 0)
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
